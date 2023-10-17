cbb6b3132f6a0e354a93e94c578e1c74af858c68
Itshnic72 <rv920490@gmail.com>
2023-10-17 15:39:49 +0500

    update lesson 9

--- Lesson_9/Program.cs
+++ Lesson_9/Program.cs
@@ -6,7 +6,7 @@ int Input(string text)
 	Console.Write($"{text} ");
 	return Convert.ToInt32(Console.ReadLine());
 }
-/* void Number(int m, int n)
+void Number(int m, int n)
 {
 	if (m != n)
 	{
@@ -17,26 +17,29 @@ int Input(string text)
 }
 int m = Input("От ->");
 int n = Input("-> До");
-Console.Write($"{n} -> "); */
-/* Number(m, n);
+Console.Write($"{n} -> ");
 
+Number(m, n);
+Console.WriteLine();
+/* 
 Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
 453 -> 12
 45 -> 9 */
-/* int SumNum(int N)
+
+int SumNum(int number)
 {
-	if (N == 0) return N;
-	return N % 10 + SumNum(N / 10);
+	if (number == 0) return number;
+	return number % 10 + SumNum(number / 10);
 }
 
-int N = Input("Введите число: ");
-System.Console.WriteLine(SumNum(N)); */
+int number = Input("Введите число: ");
+System.Console.WriteLine(SumNum(number));
 
 /* Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
 A = 3; B = 5-> 243(3⁵)
 A = 2; B = 3-> 8 */
 
-/* int A = Input("Введите число : ");
+int A = Input("Введите число : ");
 int B = Input("В какую степень его возвести: ");
 int Pow(int A, int B)
 {
@@ -45,35 +48,37 @@ int Pow(int A, int B)
 	(B == 0) ? 1 : 0;
 
 }
-Console.WriteLine(Pow(A, B)); */
+Console.WriteLine(Pow(A, B));
 
 /* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
-/* int Numbers(int start, int end)
+int Numbers(int start, int end)
 {
 	Console.Write($"{start}, ");
 	return start = start > end + 1 ? Numbers(start - 1, end) : end;
 }
 
-int n = Input("Напишите значение N");
-int m = 1;
-Console.Write($"N={n} -> ");
-Console.Write(Numbers(n, m)); */
+int start = Input("Напишите значение N");
+int end = 1;
+Console.Write($"N={start} -> ");
+Console.Write(Numbers(start, end));
+Console.WriteLine();
 
 /* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15-> 120
 M = 4; N = 8. -> 30 */
 
-/* int m = Input("Задайте число M = ");
-int n = Input("Задайте число N = ");
+int M = Input("Задайте число M = ");
+int N = Input("Задайте число N = ");
 
 int SummMN(int m, int n)
 {
 	return m <= n ? SummMN(m + 1, n) + m : 0;
 }
 
-Console.Write($"-> {SummMN(m, n)}"); */
+Console.WriteLine($"-> {SummMN(M, N)}");
+
 
 /* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 m = 2, n = 3->A(m, n) = 9
@@ -92,4 +97,4 @@ int Akkerman(int oneNumber, int twoNumber)
 	else
 		return Akkerman(oneNumber - 1, Akkerman(oneNumber, twoNumber - 1));
 }
-Console.Write(Akkerman(oneNumber, twoNumber));
+Console.WriteLine(Akkerman(oneNumber, twoNumber));
